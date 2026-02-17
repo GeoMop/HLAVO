09cb2f8434dd93fcea22621e3d7b644e259f3e80
jstebel <jan.stebel@tul.cz>
2026-02-17 12:26:18 +0100

    Once more add test for ToyProblem.

---
+++ tests/soil_parflow/test_parflow_model.py
@@ -0,0 +1,44 @@
+from hlavo.soil_parflow.parflow_model import ToyProblem
+
+model_config = {
+        "init_pressure": [-40, -100],  # bottom, top
+        "static_params": {
+            "ComputationalGrid": {
+                "Lower.Z": -135,
+                "DZ": 9,
+                "NZ": 15,
+            },
+            "BCPressure_bottom": 0,
+        },
+        "params": {
+            "vG_K_s": "Geom.domain.Perm.Value",
+            "vG_n": [
+                "Geom.domain.Saturation.N",
+                "Geom.domain.RelPerm.N",
+            ],
+            "vG_Th_s": "Geom.domain.Saturation.SSat",
+            "vG_Th_r": "Geom.domain.Saturation.SRes",
+            "vG_alpha": [
+                "Geom.domain.Saturation.Alpha",
+                "Geom.domain.RelPerm.Alpha",
+            ],
+        },
+    }
+
+# Model initialization
+toy = ToyProblem(model_config, workdir="output-toy")
+pressure = toy.make_linear_pressure(model_config)
+
+# Model parameters
+precipitation_flux = -0.0166
+stop_time = 200
+model_time_step = 0.025
+
+# Run and postprocess
+toy.run(init_pressure=pressure,
+        precipitation_value=precipitation_flux,
+        start_time=0,
+        stop_time=stop_time,
+        time_step=model_time_step)
+
+toy.save_pressure("pressure.png")
