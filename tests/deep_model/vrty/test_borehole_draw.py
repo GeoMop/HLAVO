2543bc66cd5d995ced5d9fd543e81d859dda7923
David Flanderka <t426.001@centrum.cz>
2026-03-05 15:20:17 +0100

    Create tests of conversion functions.

---
+++ tests/deep_model/vrty/test_borehole_draw.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author:   David Flanderka
+
+import sys
+sys.path.append('../../../hlavo/deep_model/vrty')
+import conversion_data
+
+
+def main(argv):
+    if (len(argv) != 3):
+        argv = ["../../../hlavo/deep_model/vrty/25_09_27_Odbery_Uhelna.xlsx", "List1", "Odbery_uprav.csv"]
+        # temporary hack, set args automatically if they are not set
+        #print("Invalid number of input args, must be 3! ")
+        #sys.exit(1)
+
+    excel_df = conversion_data.read_draw(xls_file=argv[0], sheetname=argv[1])
+    print(excel_df)
+    conversion_data.csv_output(csv_file=argv[2], df=excel_df)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ tests/deep_model/vrty/test_borehole_water_level.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author:   David Flanderka
+
+
+
+import sys
+sys.path.append('../../../hlavo/deep_model/vrty')
+import conversion_data
+
+def main():
+    final_df = conversion_data.read_water_level()
+    print(final_df)
+
+if __name__ == "__main__":
+   main()
