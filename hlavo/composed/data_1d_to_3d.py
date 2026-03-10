e2e8c3d6f3fcfad73d679d9e2afe35939cfe5aef
martinspetlik <martin.spetlik>
2026-03-10 16:11:34 +0100

    data 1d to 3d 'improvement'

--- hlavo/composed/data_1d_to_3d.py
+++ hlavo/composed/data_1d_to_3d.py
@@ -5,8 +5,8 @@ from typing import Any
 
 @dataclass(slots=True)
 class Data1DTo3D:
-    date_time: np.datetime64
+    date_time: np.datetime64['m']
     site_id: int
     longitude: float
     latitude: float
-    velocity: Any
+    velocity: Any # Units not yet known
