778b2fe4f4afeaf3759138119757e5c6b1a46c32
David Flanderka <t426.001@centrum.cz>
2026-03-30 13:10:05 +0200

    Draft of filtering data.

---
+++ hlavo/ingress/moist_profile/data_filter.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# author:   David Flanderka
+
+
+
+import sys
+from pathlib import Path
+import pandas as pd
+import logging
+import zarr_fuse as zf
+from dotenv import load_dotenv
+import polars as pl
+import matplotlib.pyplot as plt
+
+logger = logging.getLogger(__name__)
+
+
+def _create_work_dir():
+    """
+    Create workdir if doesn't exist, return path
+    """
+    script_dir = Path(__file__).parent
+    workdir = script_dir / "workdir"
+    workdir.mkdir(exist_ok=True)
+    return workdir
+
+
+def _open_zarr_schema():
+    script_dir = Path(__file__).parent
+    root_path = script_dir / "../../.."
+    file_path = root_path / ".secrets_env"
+    load_dotenv(dotenv_path=file_path)
+
+    schema_path = script_dir / "profile_schema.yaml"
+    return zf.open_store(schema_path)
+
+
+def read_data():
+    """
+    Read moist data from zarr_fuse storage.
+
+    Returns dataframe contains data of moisture dataset
+    """
+    root_node = _open_zarr_schema()
+    water_level_node = root_node['Uhelna']['profiles']
+    #print(water_level_node.dataset)
+
+    ds = water_level_node.dataset['moisture']
+    df = ds.to_dataframe()
+    return df
+
+
+def apply_filter(df, site_id, depth_level, out_file):
+    """
+    IN PROGRESS. Plot data of one side and depth level.
+
+    Param   df            dataframe of moisture data
+    Param   site_id       Index of site
+    Param   depth_level   Order of depth level
+    Returns pandas:DataFrame
+    """
+    df_filtered = df[
+        (df["site_id"] == site_id) &
+        (df["depth_level"] == depth_level)
+        ]
+    #df_filtered = df_filtered.to_pandas()
+    ax = df_filtered.plot(
+        x="date_time",
+        y=["moisture"],
+        figsize=(10, 5)
+    )
+
+    ax.set_title("Moisture data of site: '" + site_id + "', level: '" + depth_level + "'")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Moisture")
+    ax.grid(True)
+    ax.get_figure()
+
+    workdir = _create_work_dir()
+    full_path = workdir / out_file
+
+    plt.tight_layout()
+    plt.savefig(full_path)
+    plt.close()
+
+
+def main():
+    final_df = read_data()
+    print(final_df)
+
+
+if __name__ == "__main__":
+   main()
