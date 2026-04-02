84b2c97e4ec9f6ed6a7f2c64c6d4b136b33468e3
David Flanderka <t426.001@centrum.cz>
2026-04-02 09:14:33 +0200

    Implementation of multiple plot without filter.
    - plots set of graphs given by lists of site_id and depth_level

--- hlavo/ingress/moist_profile/data_filter.py
+++ hlavo/ingress/moist_profile/data_filter.py
@@ -11,7 +11,14 @@ import logging
 import zarr_fuse as zf
 from dotenv import load_dotenv
 import polars as pl
+import matplotlib
+matplotlib.use('Agg')  # for interactive graphs
+
 import matplotlib.pyplot as plt
+from matplotlib.colors import TwoSlopeNorm
+from matplotlib.backends.backend_pdf import PdfPages
+
+matplotlib.rcParams['hatch.linewidth'] = 6
 
 logger = logging.getLogger(__name__)
 
@@ -36,6 +43,33 @@ def _open_zarr_schema():
     return zf.open_store(schema_path)
 
 
+def _plot_single_site_level(df, site_id, depth_level):
+    """
+    Plot graph of one site_id and depth_level.
+
+    Param   df            dataframe of moisture data
+    Param   site_id       Index of site
+    Param   depth_level   Order of depth level
+    Returns plt object
+    """
+    df_filtered = df[
+        (df["site_id"] == site_id) &
+        (df["depth_level"] == depth_level)
+        ]
+    #print(df_filtered)
+    ax = df_filtered.plot(
+        x="date_time",
+        y=["moisture"],
+        figsize=(10, 5)
+    )
+
+    ax.set_title("Moisture data of site: '" + str(site_id) + "', level: '" + str(depth_level) + "'")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Moisture")
+    ax.grid(True)
+    return ax.get_figure()
+
+
 def read_data():
     """
     Read moist data from zarr_fuse storage.
@@ -50,39 +84,29 @@ def read_data():
     return df
 
 
-def apply_filter(df, site_id, depth_level, out_file):
+def apply_filter(df, site_ids, depth_levels, out_file):
     """
     IN PROGRESS. Plot data of one side and depth level.
 
     Param   df            dataframe of moisture data
-    Param   site_id       Index of site
-    Param   depth_level   Order of depth level
-    Returns pandas:DataFrame
+    Param   site_ids      List of indexes of site
+    Param   depth_levels  List of orders of depth levels
+    Param   out_file      Name of output pdf file
     """
     df_pandas = df.to_pandas()
-    df_filtered = df_pandas[
-        (df_pandas["site_id"] == site_id) &
-        (df_pandas["depth_level"] == depth_level)
-        ]
-    print(df_filtered)
-    ax = df_filtered.plot(
-        x="date_time",
-        y=["moisture"],
-        figsize=(10, 5)
-    )
-
-    ax.set_title("Moisture data of site: '" + str(site_id) + "', level: '" + str(depth_level) + "'")
-    ax.set_xlabel("Date")
-    ax.set_ylabel("Moisture")
-    ax.grid(True)
-    ax.get_figure()
 
     workdir = _create_work_dir()
-    full_path = workdir / out_file
+    full_out_file = out_file + ".pdf"
+    full_path = workdir / full_out_file
+    pdf = PdfPages(full_path)
 
-    plt.tight_layout()
-    plt.savefig(full_path)
-    plt.close()
+    for site_id in site_ids:
+        for depth_level in depth_levels:
+            fig = _plot_single_site_level(df_pandas, site_id, depth_level)
+            pdf.savefig(fig, bbox_inches="tight")
+            plt.close(fig)
+
+    pdf.close()
 
 
 def main():
--- tests/ingress/moist_profile/test_data_filter.py
+++ tests/ingress/moist_profile/test_data_filter.py
@@ -4,15 +4,15 @@
 
 import sys
 from hlavo.ingress.moist_profile import data_filter
-from hlavo.ingress import well_data   # for csv_output()
 
 
 def test_data_filter():
     final_df = data_filter.read_data()
     print(final_df)
-    # well_data.csv_output("moist_out.csv", final_df)
 
-    data_filter.apply_filter(final_df, 1, 1, "plot.png")
+    site_ids = [1, 2, 3]
+    depth_levels = [1, 2, 3, 4, 5]
+    data_filter.apply_filter(final_df, site_ids, depth_levels, "plot_all")
 
 if __name__ == "__main__":
     test_data_filter()
